8adfd47e8ed430596f48a30b9f2efce40ae60a4c
Mohit Singh <mohit.singh@aditiconsulting.com>
2026-06-15 05:14:48 +0530

    Langgraph learning about the agent workflows and all with projects and excercises

---
+++ Langgraph/MCP-server/chatbot_mcp.py
@@ -0,0 +1,66 @@
+from langgraph.graph import StateGraph, START
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_community.tools import DuckDuckGoSearchRun
+from typing import TypedDict,Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_core.tools import tool
+import asyncio
+from langchain_mcp_adapters.client import MultiServerClient
+
+load_dotenv()
+llm = ChatGroq(model = "llama-3.3-70b-versatile")
+
+client = MultiServerClient(
+    {
+        "arith":{
+            "transport":"stdio",
+            "command":"python3",
+            "args":["/users"]
+        },
+        "expense":{
+            "transport":"streamable_http",
+            "url": "https://splendid-gold-dingo.fastmcp.app/mcp"
+        }
+    }
+)
+
+#state
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+async def build_graph():
+    tools = await client.get_tools()
+    print(tools)
+
+    llm_with_tools = llm.bind_tools(tools)
+    # nodes
+    async def chat_node(state: ChatState):
+        messages = state["messages"]
+        response= await llm_with_tools.ainvoke(messages)
+        return {'messages':[response]}
+    
+    tool_node = ToolNode(tools)
+
+    graph = StateGraph(ChatState)
+    graph.add_node("chat_node", chat_node)
+    graph.add_tools("tools",tool_node)
+
+    graph.add_edge(START, "chat_node")
+    graph.add_conditional_edges("chat_node", tools_condition)
+    graph.add_edge("tools", "chat_node")
+
+    chatbot = graph.compile()
+    return chatbot
+
+async def main():
+    chatbot =await build_graph()
+
+    # running the graph
+    result = await chatbot.invoke({"messages":[HumanMessage(content="Give me all my expenses for the month of Nov from 1 Nov to 30 Nov")]})
+    print(result['messages'][-1].content)
+
+if __name__ == '__main__':
+    asyncio(main())
